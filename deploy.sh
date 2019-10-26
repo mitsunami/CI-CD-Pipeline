8bc9229852f8c5d76eb2a6bb0df9b5502f376640
Koki Mitsunami <k.mitsunami@gmail.com>
2019-10-26 06:26:16 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-kubectl set image deployment/cicdpipeline-deployment cicdpipeline=kmitsunami/cicdpipeline
+build_number=4
+kubectl set image deployment/cicdpipeline-deployment cicdpipeline=kmitsunami/cicdpipeline:$build_number
